6196730ca1675a24714ae71ae5bf3cd67fd855b4
Raul Teixeira <raul.teixeira@camara.leg.br>
2015-05-05 19:23:41 -0300

    Started configuring restangular to implement timeentries.

--- app/modules/hours/js/hours_controllers.js
+++ app/modules/hours/js/hours_controllers.js
@@ -11,39 +11,41 @@ angular.module('zideco.hourscontrollers', ['zideco.services', 'ui.calendar'])
   console.log('Got to hours main controller');
   $scope.summaryEventSources = [];
 
-  var date = new Date();
-  var d = date.getDate();
-  var m = date.getMonth();
-  var y = date.getFullYear();
+  // var date = new Date();
+  // var d = date.getDate();
+  // var m = date.getMonth();
+  // var y = date.getFullYear();
   
-  $scope.events = [{
-    title: 'All Day Event',
-    start: new Date(y, m, 1)
-  }, {
-    title: 'Long Event',
-    start: new Date(y, m, d - 5),
-    end: new Date(y, m, d - 2)
-  }, {
-    id: 999,
-    title: 'Repeating Event',
-    start: new Date(y, m, d - 3, 16, 0),
-    allDay: false
-  }, {
-    id: 999,
-    title: 'Repeating Event',
-    start: new Date(y, m, d + 4, 16, 0),
-    allDay: false
-  }, {
-    title: 'Birthday Party',
-    start: new Date(y, m, d + 1, 19, 0),
-    end: new Date(y, m, d + 1, 22, 30),
-    allDay: false
-  }, {
-    title: 'Click for Google',
-    start: new Date(y, m, 28),
-    end: new Date(y, m, 29),
-    url: 'http://google.com/'
-  }];
+  $scope.events = [];
+  // {
+  //   title: 'All Day Event',
+  //   start: new Date(y, m, 1)
+  // }, {
+  //   title: 'Long Event',
+  //   start: new Date(y, m, d - 5),
+  //   end: new Date(y, m, d - 2)
+  // }, {
+  //   id: 999,
+  //   title: 'Repeating Event',
+  //   start: new Date(y, m, d - 3, 16, 0),
+  //   allDay: false
+  // }, {
+  //   id: 999,
+  //   title: 'Repeating Event',
+  //   start: new Date(y, m, d + 4, 16, 0),
+  //   allDay: false
+  // }, {
+  //   title: 'Birthday Party',
+  //   start: new Date(y, m, d + 1, 19, 0),
+  //   end: new Date(y, m, d + 1, 22, 30),
+  //   allDay: false
+  // }, {
+  //   title: 'Click for Google',
+  //   start: new Date(y, m, 28),
+  //   end: new Date(y, m, 29),
+  //   url: 'http://google.com/'
+  // }
+  // ];
 
   // $scope.eventSource = {
   //             url: "http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic",
--- app/modules/hours/js/hours_repo_services.js
+++ app/modules/hours/js/hours_repo_services.js
@@ -1,20 +1,53 @@
-
 /**
  * Created by raul on 23/05/14.
  */
 'use strict';
 
 angular.module('zideco.hours.reposervices', [])
-  .constant('version', '0.0.1')
-
-// .factory('hoursResourceService', ['$http', '$q', '$log', '$location', 'Restangular', function ($http, $q, $log, $location, Restangular) {
-
-//   //return API
-
-//   return {
-//   };
+    .constant('version', '0.0.1')
 
 
-// }])
+.factory('hoursResourceService', ['$http', '$q', '$log', '$location', 'Restangular', function($http, $q, $log, $location, Restangular) {
+    var resourceTimeEntry = 'timeentry';
+
+    // function resolveErroComPromessa(msg) {
+    //    var deferred = $q.defer();
+
+    //    deferred.reject({
+    //       status: 'error',
+    //       msg: msg,
+    //       //para manter compatibilidade com o restangular
+    //       data: {
+    //          err: msg
+    //       }
+    //    });
+    //    return deferred.promise;
+    // }
+
+    var getTimeEntries = function(filter) {
+        var restObject = Restangular.all(resourceTimeEntry);
+        return restObject.getList(filter);
+    };
+
+    var saveTimeEntry = function(timeEntry) {
+        if (_.isUndefined(timeEntry)) {
+            throw new Error('Need object to save.');
+        }
+
+        if (Restangular.configuration.isRestangularized(timeEntry)) {
+            return timeEntry.save();
+        }
+
+        return Restangular.all(resourceTimeEntry).post(timeEntry);
+    };
+
+    //return API
+    return {
+        getTimeEntries: getTimeEntries,
+        saveTimeEntry: saveTimeEntry
+    };
+
+
+}])
 
 ;
