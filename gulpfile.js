252920852741f82d9933be011a7720b9c81ff28b
raul <raul.teixeira@gmail.com>
2015-11-10 13:22:13 -0200

    Gulp file and Sublime files

--- gulpfile.js
+++ gulpfile.js
@@ -30,12 +30,12 @@ function buildVersionString() {
     var jenkinsTag = process.env.BUILD_TAG;
     if (!jenkinsTag || jenkinsTag === '') {
         var hoje = new Date();
-        var day = ('00' + hoje.getUTCDate()).slice (-2);
-        var month = ('00' + (hoje.getUTCMonth() + 1)).slice (-2);
-        var year = ('0000' + hoje.getUTCFullYear()).slice (-4);
-        var hour = ('00' + hoje.getUTCHours()).slice (-2);
-        var minute = ('00' + hoje.getUTCMinutes()).slice (-2);
-        var second = ('00' + hoje.getUTCSeconds()).slice (-2);
+        var day = ('00' + hoje.getUTCDate()).slice(-2);
+        var month = ('00' + (hoje.getUTCMonth() + 1)).slice(-2);
+        var year = ('0000' + hoje.getUTCFullYear()).slice(-4);
+        var hour = ('00' + hoje.getUTCHours()).slice(-2);
+        var minute = ('00' + hoje.getUTCMinutes()).slice(-2);
+        var second = ('00' + hoje.getUTCSeconds()).slice(-2);
 
 
         jenkinsTag = os.hostname() + '-UTC-' + year + '-' + month + '-' + day + '_' + hour + ':' + minute + ':' + second;
@@ -45,28 +45,28 @@ function buildVersionString() {
 
 
 // Tarefa usada so para testar se os GLOBS de um diretorio estao corretos.
-gulp.task('testcopy', function () {
+gulp.task('testcopy', function() {
     return gulp.src('./app/**/e2e/**/*')
         .pipe(gulp.dest(paths.build));
 });
 
 // Tarefa limpa diretorio de "build"
-gulp.task('clean', function () {
+gulp.task('clean', function() {
     rimraf.sync(paths.build);
 });
 
-gulp.task('cleanE2eReports', function () {
+gulp.task('cleanE2eReports', function() {
     rimraf.sync(paths.testes.reports);
 });
 
 //Roda testes e2e
-gulp.task('e2e', ['cleanE2eReports'], function () {
+gulp.task('e2e', ['cleanE2eReports'], function() {
     gulp.src([paths.testes.e2e])
         .pipe(protractor({
             configFile: 'protractor-conf.js'
-//    ,    args: ['--baseUrl', 'http://127.0.0.1:8000']
+                //    ,    args: ['--baseUrl', 'http://127.0.0.1:8000']
         }))
-        .on('error', function (e) {
+        .on('error', function(e) {
             throw e;
         });
 });
@@ -75,22 +75,22 @@ gulp.task('e2e', ['cleanE2eReports'], function () {
 
 // Para bibliotecas de terceiros:
 // Compila Sass, mescla com css normal e output para app.css
-gulp.task('VendorCSS', ['CopiaWebFonts'], function () {
+gulp.task('VendorCSS', ['CopiaWebFonts'], function() {
 
     var sassFiles = gulp.src(paths.vendorsass);
     var cssFiles = gulp.src(paths.vendorcss);
 
     return es.merge(
-        sassFiles.pipe(sass()),
-        cssFiles)
+            sassFiles.pipe(sass()),
+            cssFiles)
         .pipe(changed(paths.build))
         .pipe(concat('lib.css'))
         .pipe(minifyCSS())
 
-        .pipe(gulp.dest(paths.build));
+    .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('CopiaWebFonts', function () {
+gulp.task('CopiaWebFonts', function() {
     gulp.src('./app/assets/fonts/*.*')
         .pipe(gulp.dest(paths.buildfonts));
 
@@ -103,25 +103,25 @@ gulp.task('CopiaWebFonts', function () {
 
 //Compila Sass, mescla com css normal e output para app.css
 //Tambem prefixa para 2 versoes e minifica.
-gulp.task('AppCSS', function () {
+gulp.task('AppCSS', function() {
 
     var sassFiles = gulp.src(paths.appsass);
     var cssFiles = gulp.src(paths.appcss);
     return es.merge(
-        sassFiles.pipe(sass()),
-        cssFiles)
-//        .pipe(changed(paths.build))
+            sassFiles.pipe(sass()),
+            cssFiles)
+        //        .pipe(changed(paths.build))
         .pipe(concat('app.css'))
         .pipe(autoprefixer('last 2 versions'))
         .pipe(minifyCSS())
 
-        .pipe(gulp.dest(paths.build));
+    .pipe(gulp.dest(paths.build));
 });
 
 
 
 //minifica imagens.
-gulp.task('imagemin', function () {
+gulp.task('imagemin', function() {
     var imgSrc = paths.images,
         imgDst = paths.build;
 
@@ -134,45 +134,47 @@ gulp.task('imagemin', function () {
 
 
 //Combine e minifica os arquivos JS da aplicacao.
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
     var versionNumber = buildVersionString();
 
     return es.merge(
         gulp.src(paths.envConfigFiles.production)
-            .pipe(replace('<!--BuildVersion-->', versionNumber)),
+        .pipe(replace('<!--BuildVersion-->', versionNumber)),
         gulp.src(paths.scripts))
 
-        .pipe(jshint())
+    .pipe(jshint())
         .pipe(jshint.reporter('default'))
-//        .pipe(stripDebug())
-//    .pipe(ngmin())
-//    .pipe(uglify({mangle: false}))
+        //        .pipe(stripDebug())
+        //    .pipe(ngmin())
+        //    .pipe(uglify({mangle: false}))
         .pipe(concat('app.js'))
         .pipe(gulp.dest(paths.build));
 });
 
 //combine all template files of the app into a js file and put into cache.
-gulp.task('templates', function () {
+gulp.task('templates', function() {
     gulp.src(paths.templates)
-        .pipe(angularTemplatecache('templates.js', {standalone: true}))
+        .pipe(angularTemplatecache('templates.js', {
+            standalone: true
+        }))
         .pipe(gulp.dest(paths.build));
 });
 
 
 // Junto JS de vendors, arranca debug e minifica
-gulp.task('vendorJS', function () {
+gulp.task('vendorJS', function() {
     //concatenate vendor JS files
     gulp.src(paths.vendorJS)
         .pipe(concat('lib.js'))
         .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('copyMapFiles', function () {
+gulp.task('copyMapFiles', function() {
     gulp.src(paths.vendorMapFiles)
         .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('copy-index', function () {
+gulp.task('copy-index', function() {
     //Aqui, vamos remover um pedaco do index.html e substituir com o trecho abaixo.
     //perceba que a ORDEM dos .js e importante. O lib precisa ir primeiro, pois traz todas as dependencias (angular, etc.)
     //em seguida vem o app que traz nossas definicoes.
@@ -197,12 +199,12 @@ gulp.task('copy-index', function () {
         .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', function() {
     gulp.watch([
         'build/**/*.html',
         'build/**/*.js',
         'build/**/*.css'
-    ], function (event) {
+    ], function(event) {
         return gulp.src(event.path)
             .pipe(connect.reload());
     });
@@ -216,7 +218,7 @@ gulp.task('watch', function () {
 
 
 //Abaixo sao tarefas que usadas para DESENVOLVIMENTO.
-gulp.task('copyImagesNoMin', function () {
+gulp.task('copyImagesNoMin', function() {
     var imageDestinationDir = paths.build + '/assets/images';
 
     gulp.src('./app/assets/images/*.*')
@@ -225,11 +227,13 @@ gulp.task('copyImagesNoMin', function () {
 
 gulp.task('debugsass', function() {
     var sassFiles = gulp.src('./app/assets/style/**/*.scss');
-    sassFiles.pipe(sass({errLogToConsole: true}))
+    sassFiles.pipe(sass({
+            errLogToConsole: true
+        }))
         .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('copyMockFiles', function () {
+gulp.task('copyMockFiles', function() {
 
     var mockDestino = paths.build + '/mockdata';
 
@@ -238,7 +242,7 @@ gulp.task('copyMockFiles', function () {
         .pipe(gulp.dest(mockDestino));
 });
 
-gulp.task('watchSTYLE', function () {
+gulp.task('watchSTYLE', function() {
     gulp.watch(paths.appcss, ['AppCSS']);
     gulp.watch(paths.appsass, ['AppCSS']);
     gulp.watch(paths.vendorcss, ['VendorCSS']);
@@ -246,14 +250,14 @@ gulp.task('watchSTYLE', function () {
     // gulp.watch(paths.images, ['ProcessaStilosParaDesenvolvimento']);
 });
 
-gulp.task('watchMock', function () {
+gulp.task('watchMock', function() {
     gulp.watch(paths.mockFiles, ['copyMockFiles']);
 
 });
 
 
 //Depois ver forma melhor de modularizar isso.
-var fileWritingReporter = map(function (file, cb) {
+var fileWritingReporter = map(function(file, cb) {
     if (!file.jshint.success) {
         var fs = require('fs');
         var originalFileName = file.path.split('/').splice(-1)[0];
@@ -265,7 +269,7 @@ var fileWritingReporter = map(function (file, cb) {
             fs.mkdirSync(logFileDir);
         }
         //Create new file.
-        fs.writeFile(logFileName, 'Erros para: ' + file.path, function (err) {
+        fs.writeFile(logFileName, 'Erros para: ' + file.path, function(err) {
             if (err) {
                 console.log(err);
             }
@@ -273,10 +277,12 @@ var fileWritingReporter = map(function (file, cb) {
 
 
         //Grava erros.
-        file.jshint.results.forEach(function (err) {
+        file.jshint.results.forEach(function(err) {
             if (err) {
                 var msg = '\n' + originalFileName + ': line ' + err.error.line + ', col ' + err.error.character + ', code ' + err.error.code + ', ' + err.error.reason;
-                fs.appendFile(logFileName, msg, {'flag': 'a'}, function (werr) {
+                fs.appendFile(logFileName, msg, {
+                    'flag': 'a'
+                }, function(werr) {
                     if (werr) {
                         console.log(werr);
                     }
@@ -291,26 +297,24 @@ var fileWritingReporter = map(function (file, cb) {
 //Cria tarefa lint
 
 
-gulp.task('lint', function () {
+gulp.task('lint', function() {
     return gulp.src(paths.scripts)
         .pipe(jshint('.jshintrc'))
         .pipe(logCapture.start(console, 'log'))
         .pipe(jshint.reporter('jslint_xml'))
         .pipe(logCapture.stop('xml'))
-        .pipe(gulp.dest('lint-reports'))
-        ;
+        .pipe(gulp.dest('lint-reports'));
 
 
 });
 
 //Creio que poderia usar o logCapture acima para gerar os arquivos.
 //O ideal contudo, creio que seria criar um reporter que aceitasse outro reporter e gravasse o resultado para um arquivo.
-gulp.task('lint-dev', function () {
+gulp.task('lint-dev', function() {
     return gulp.src(paths.scripts)
         .pipe(jshint('.jshintrc'))
         .pipe(fileWritingReporter)
-        .pipe(jshint.reporter(stylish))
-        ;
+        .pipe(jshint.reporter(stylish));
 });
 
 
@@ -329,7 +333,7 @@ gulp.task('connect-dev', connect.server({
 
 
 
-gulp.task('default', ['lint','genProductionBuild']);
+gulp.task('default', ['lint', 'genProductionBuild']);
 gulp.task('genProductionBuild', ['genNoServe', 'copyMockFiles']);
 //gulp.task('deploy', ['genProductionBuild']);
 
@@ -339,5 +343,3 @@ gulp.task('genAndServeNoWatch', ['genNoServe', 'connect']);
 gulp.task('genAndServeAndWatch', ['genAndServeNoWatch', 'watch']);
 gulp.task('desenv', ['copyImagesNoMin', 'copyMockFiles', 'VendorCSS', 'AppCSS', 'watchSTYLE', 'watchMock']);
 gulp.task('serve-desenv', ['desenv', 'connect-dev']);
-
-
